fd604b287f23c3bdabb4042a5addf3badc0a7239
Flakymsk <1andreevsasha1@gmail.com>
2026-07-10 17:09:38 +0300

    Create A - Егор строит НВП.c

---
+++ 3. Динамическое программирование/A - Егор строит НВП.c
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+#define all(x) begin(x), end(x)
+ll mod = 1e9+7;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = n; i > 0; --i) {
+        cout << i << ' ';
+    }
+}
